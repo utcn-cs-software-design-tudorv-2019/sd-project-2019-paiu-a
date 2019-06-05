434e5db685c4f5be998366e804061351ad969961
Paiu Alexandra <a.dream.alexandra@gmail.com>
2019-06-05 10:28:52 +0300

    New

---
+++ code/src/Data/Book.java
@@ -0,0 +1,14 @@
+package Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "idBook")
+    private int idBook;
+
+}
